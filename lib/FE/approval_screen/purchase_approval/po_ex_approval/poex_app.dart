a8a008dbd7b2f157961d214e54444f37794c13b8
Fransisco Thorion <fransiscothorion@gmail.com>
2023-11-27 14:33:30 +0700

    update ios

--- lib/FE/approval_screen/purchase_approval/po_ex_approval/poex_app.dart
+++ lib/FE/approval_screen/purchase_approval/po_ex_approval/poex_app.dart
@@ -96,7 +96,7 @@ class _PoExAppState extends State<PoExApp> {
               child: LiquidPullToRefresh(
                 onRefresh: getDataa2,
                 color: HexColor("#F4A62A"),
-                height: 150,
+                height: 140,
                 showChildOpacityTransition: false,
                 child: ListView(
                   children: [
@@ -131,7 +131,15 @@ class _PoExAppState extends State<PoExApp> {
                                 return const Center(
                                     child: Column(
                                   children: [
-                                    Text('Loading...'),
+                                    DefaultTextStyle(
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 20,
+                                      ),
+                                      child: Text(
+                                        'Loading...',
+                                      ),
+                                    ),
                                     SizedBox(
                                       height: 20,
                                     ),
@@ -139,7 +147,13 @@ class _PoExAppState extends State<PoExApp> {
                                     SizedBox(
                                       height: 20,
                                     ),
-                                    Text('Please Kindly Waiting...'),
+                                    DefaultTextStyle(
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 20,
+                                        ),
+                                        child:
+                                            Text('Please Kindly Waiting...')),
                                   ],
                                 ));
                               } else {
